cb0fb24d6c817879c86d034edabc50893e59319d
urhlyan <kohaliana@gmail.com>
2025-11-07 20:22:18 +0800

    Convert Arduino Input Logic

--- 04-final-project/sensor-jam-template/sketch.js
+++ 04-final-project/sensor-jam-template/sketch.js
@@ -13,115 +13,40 @@ function setup() {
   colorMode(HSB,360,100,100,100);
   createCanvas(windowWidth, windowHeight);
   port = createSerial(9600); // creates the Serial Port
-
   // Connection helpers
   connectBtn = createButton("Connect to Arduino");
   connectBtn.position(20, 20);
   connectBtn.mousePressed(connectBtnClick);
-  
 }
-class Balls{
-  constructor(x,y,r){
-    this.x= x;
-    this.y = y;
-    this.r=r;
-  }
-    drawBall(){
-      ellipse(this.x,this.y,this.r);
-    }
-    updateBall(){
-      // this.r-=10;
-    }
-  }
+
+//== MY CODE==
 
 function draw() {
   background(100);
-  
-  fill(updateCol,100,100);
+  fill(0);
 
-  ellipse(width / 2,height/2 + updateCol, circleSize);
 
-  ballsArray.push( new Balls(width/2,height/2,updateCol))
-
-  for(let i = ballsArray.length - 1; i >= 0; i--){
-        ballsArray[i].drawBall();
-        ballsArray[i].updateBall();
-  }
-
-  for (let i=0;i<updateCol;i++){
-    ballsArray.push(new Balls(random(width),random(height),updateCol))
-  }
-
-  if(ballsArray.length>100){
-    ballsArray.splice(0,100);
-  }
-  // console.log(ballsArray.length);
   // Receive data from Arduino
-
-
   if (port.opened()) {
+    // read value from port and convert to float 
+
     let recvStr = port.readUntil("\n");
     if(recvStr && recvStr.includes("cm")){
-      let cleanedStr= recvStr.replace("cm","").trim();
-      let sensorVal= parseFloat(cleanedStr);
-
+      let cleanedStr= recvStr.replace("cm","").trim(); //remove cm
+      let sensorVal= int(cleanedStr); //convert to float
       if (!isNaN(sensorVal)){
+        updateCol=sensorVal;
         console.log("sensorVal: "+sensorVal);
+        console.log("updateCol: " +updateCol);
       }else{
         console.warn("NaN detected - cleanedStr was:", cleanedStr);
       }
     }
-    // console.log(recvStr);
-    // let cleanedStr= recvStr.replace("cm","");
+    console.log(updateCol);
+    ellipse(width/2,height/2,updateCol);
     
-    // let sensorVal= float(cleanedStr);
-    
-    //    console.log("sensorVal:"+ sensorVal);
-    
-    
-    // Only log data that has information, not empty signals
-    //console.log(sensorVal[0]);
-    
-
-    // if (recvStr[0] == 'D') {
-    //   console.log(" inside if condition ")
-
-      // Once you verify data is coming in,
-      // disable logging to improve performanceide
-
-      //console.log(">>>>>");
-      //console.log(recvStr.substring(10,recvStr.length-4));
-
-      // sensorVal = int(recvStr.substring(10,recvStr.length-4));
-      // console.log(sensorVal)
-
-
-      // OPTION 1:
-      // Update circle's size with sensor's data directly
-      // Reduce delay() value in Ardiuno to get smoother changes
-
-      // use float() to convert from data from string to number
-      // circleSize = float(sensorVal);
-
-      // OPTION 2:
-      // Update circle's size using lerp() to smoothly change values
-      // This method even works with longer delay() values in Arduino
-
-      // targetSize = float(sensorVal);
-      // updateCol = float(sensorVal);
-      
-    //   if(frameCount%40===0){
-
-    //     // console.log("targetSize"+targetSize);
-    //   }
-     
-    //   // last value in lerp() controls speed of change
-    //   circleSize = lerp(circleSize, targetSize, 0.1);
-      
-    // }
   }
 }
-
 // DO NOT REMOVE THIS FUNCTION
 function connectBtnClick(e) {
   // If port is not already open, open on click,
